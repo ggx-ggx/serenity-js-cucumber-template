965b797f1b361316a2c2b13294650f564a7c64f2
gx dev <gx@g.com>
2025-04-05 00:44:49 -0400

    s2

--- features/mathjs-api/step_definitions/expressions.steps.ts
+++ features/mathjs-api/step_definitions/expressions.steps.ts
@@ -1,9 +1,15 @@
-import { Given, When, Then, DataTable } from '@cucumber/cucumber';
+import { Given, When, Then, DataTable, Before, BeforeAll } from '@cucumber/cucumber';
 import { actorCalled, Actor } from '@serenity-js/core';
 import { Ensure, equals } from '@serenity-js/assertions';
 import { GetRequest, PostRequest, Send, LastResponse } from '@serenity-js/rest';
 import { ConfigurationHelper } from '../../../src/config/ConfigurationHelper';
 import { TokenStore } from '../../../src/config/TokenStore';
+import chalk from 'chalk';
+
+// Print feature name before each scenario
+Before(function() {
+    console.log(chalk.blueBright.bold.bgWhite('\nFeature: Expressions: Basic expressions with token check'));
+});
 
 // Helper function to extract values from a DataTable
 const column = (table: DataTable, columnName: string): string[] => {
@@ -21,24 +27,28 @@ const column = (table: DataTable, columnName: string): string[] => {
 };
 
 Given(/(.*?) is a registered user/, (name: string) => {
+    console.log(chalk.whiteBright.bold.bgWhite(`\nGiven ${name} is a registered user`));
     actorCalled(name);
 });
 
 When(/(.*?) requests evaluation of (.*)/, (name: string, expression: string) => {
+    console.log(chalk.whiteBright.bold.bgWhite(`\nWhen ${name} requests evaluation of ${expression}`));
     return actorCalled(name).attemptsTo(
         Send.a(GetRequest.to(`/v4/?expr=${encodeURIComponent(expression)}`))
     );
 });
 
 Then(/(.*?) should get single result (.*)/, (name: string, expectedResult: string) => {
+    console.log(chalk.whiteBright.bold.bgWhite(`\nThen ${name} should get single result ${expectedResult}`));
     return actorCalled(name).attemptsTo(
         Ensure.that(LastResponse.body(), equals(Number(expectedResult)))
     );
 });
 
 When(/(.*?) requests evaluation of:/, (name: string, expressionsTable: DataTable) => {
+    console.log(chalk.whiteBright.bold.bgWhite(`\nWhen ${name} requests evaluation of:`));
     const expressions = column(expressionsTable, 'expression');
-    console.log('\nPOST Request - Expressions to evaluate:', expressions);
+    console.log('\nExpressions to evaluate:', expressions);
     
     return actorCalled(name).attemptsTo(
         Send.a(PostRequest.to('/v4/').with({
@@ -49,8 +59,8 @@ When(/(.*?) requests evaluation of:/, (name: string, expressionsTable: DataTable
 });
 
 Then(/(.*?) should get multiple results:/, (name: string, expectedResultsTable: DataTable) => {
+    console.log(chalk.whiteBright.bold.bgWhite(`\nThen ${name} should get multiple results:`));
     const expectedResults = column(expectedResultsTable, 'expected_result');
-    console.log('\nExpected Results:', expectedResults);
     
     // Log the full API response
     const response = LastResponse.body<any>();
@@ -62,12 +72,13 @@ Then(/(.*?) should get multiple results:/, (name: string, expectedResultsTable: 
 });
 
 Then(/(.*?) token should be logged/, (name: string) => {
+    console.log(chalk.whiteBright.bold.bgWhite(`\nThen ${name} token should be logged`));
     const tokenStore = TokenStore.getInstance();
     const tokenInfo = tokenStore.getToken(name);
     
     if (tokenInfo) {
-        console.log(`\nToken for ${name}: ${tokenInfo.token}`);
+        console.log(chalk.greenBright.bold.bgWhite(`Token for ${name}: ${tokenInfo.token}`));
     } else {
-        console.log(`\nNo token found for ${name} (no_token flag is set)`);
+        console.log(chalk.greenBright.bold.bgWhite(`No token found for ${name} (no_token flag is set)`));
     }
 });
